0152e0e49c3eac5765361858f17a79ac1f0435ff
hjibkw-ctrl <hjibkw@gmail.com>
2026-09-18 05:46:31 +0300

    Update ClassSelectionGUI.java

--- src/main/java/com/combatlevels/plugin/gui/ClassSelectionGUI.java
+++ src/main/java/com/combatlevels/plugin/gui/ClassSelectionGUI.java
@@ -23,3 +23,49 @@ public class ClassSelectionGUI {
 
     public static Inventory build() {
         Inventory inv = Bukkit.createInventory(null, SIZE, GUI_TITLE);
+
+        inv.setItem(SLOT_SWORD, createClassItem(CombatClass.SWORD));
+        inv.setItem(SLOT_MACE, createClassItem(CombatClass.MACE));
+        inv.setItem(SLOT_TNT_CART, createClassItem(CombatClass.TNT_CART));
+
+        return inv;
+    }
+
+    public static void open(Player player) {
+        player.openInventory(build());
+    }
+
+    private static ItemStack createClassItem(CombatClass combatClass) {
+        ItemStack item = new ItemStack(combatClass.getIconMaterial());
+        ItemMeta meta = item.getItemMeta();
+
+        if (meta != null) {
+            meta.setDisplayName(combatClass.getDisplayName());
+
+            List<String> lore = new ArrayList<>();
+            lore.add(combatClass.getDescription());
+            lore.add("");
+            for (String line : combatClass.getPerksLore()) {
+                lore.add(line);
+            }
+
+            meta.setLore(lore);
+            meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+            item.setItemMeta(meta);
+        }
+
+        return item;
+    }
+
+    public static int getSlotSword() {
+        return SLOT_SWORD;
+    }
+
+    public static int getSlotMace() {
+        return SLOT_MACE;
+    }
+
+    public static int getSlotTntCart() {
+        return SLOT_TNT_CART;
+    }
+}
